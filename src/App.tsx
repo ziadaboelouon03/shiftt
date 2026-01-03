d9465b3a2360d4ee6fb33a5329494b419c21e66b
ziadaboelouon03 <ziadeyes0303@gmail.com>
2026-01-04 01:19:10 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Index from "./pages/Index";
 import Auth from "./pages/Auth";
 import Admin from "./pages/Admin";
 import NotFound from "./pages/NotFound";
+import CreatePassword from '@/pages/CreatePassword';
 
 const queryClient = new QueryClient();
 
@@ -26,6 +27,7 @@ const App = () => (
               <Route path="/admin" element={<Admin />} />
               {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
               <Route path="*" element={<NotFound />} />
+              <Route path="/create-password" element={<CreatePassword />} />
             </Routes>
           </BrowserRouter>
         </TooltipProvider>
